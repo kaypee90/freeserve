e486a22ac68eecc073baaca4e47af544e90d3e1a
kwabena <kwabena@mpharma.com>
2020-03-15 22:27:03 +0000

    Run tests file

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,1 +1,17 @@
 pub mod threadpool;
+
+
+// use crate::lib::threadpool as other_threadpool;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn test_create_thread_pull_with_valid_size() {
+//         let is_valid = match other_threadpool::ThreadPool::new(4) {
+//             Ok(pool) => true,
+//             Err(_) => false,    
+//         };
+
+//         assert_eq!(is_valid, true);
+//     }
+// }
