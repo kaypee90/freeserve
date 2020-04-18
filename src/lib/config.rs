e4b324d4e504fec95d24a0339fed3fb997b4f746
kwabena <kwabena@mpharma.com>
2020-04-18 02:25:21 +0000

    Add response headers

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -11,3 +11,53 @@ impl Config {
         return app_config;
     }
 }
+
+pub enum ContentType {
+    JSON,
+    HTML,
+}
+
+impl ContentType {
+    pub fn from_app_config () -> ContentType {
+        let app_config = Config::new();
+        match app_config.use_api_view {
+            true => ContentType::JSON,
+            false => ContentType::HTML,
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        match *self {
+            ContentType::HTML => "text/html",
+            ContentType::JSON => "application/json",
+        }
+    }
+}
+
+pub struct ResponseHeaders {
+    pub content_type: Option<ContentType>,
+}
+
+impl ResponseHeaders {
+    pub fn new() -> ResponseHeaders {
+        ResponseHeaders {
+            content_type: None,
+        }
+    }
+}
+
+pub struct Response {
+    pub body: Option<Vec<u8>>,
+    pub headers: ResponseHeaders,
+    pub status: u16,
+}
+
+impl Response {
+    pub fn new() -> Response {
+        Response {
+            body: None,
+            headers: ResponseHeaders::new(),
+            status: 200,
+        }
+    }
+}
